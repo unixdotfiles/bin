1ae11905e64d6e05ed1e30a2c981abb401d53e28
Eitan Adler <lists@eitanadler.com>
2015-11-22 17:00:55 -0800

    Manually update updates

--- submodules/install.sh
+++ submodules/install.sh
@@ -2,7 +2,7 @@ set -eux
 cd "$(git rev-parse --show-toplevel)"
 git submodule init
 git submodule update
-( cd submodules/ministat &&
+[ ! -e submodules/ministat/src/ministat ] && ( cd submodules/ministat &&
   ./configure &&
   make
 )
