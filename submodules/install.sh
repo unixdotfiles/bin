9043ef3814d5d00538b25f0d5e5f0fc192a69791
Eitan Adler <lists@eitanadler.com>
2015-11-24 23:56:11 -0800

    Update submodules

--- submodules/install.sh
+++ submodules/install.sh
@@ -2,6 +2,7 @@ set -eux
 cd "$(git rev-parse --show-toplevel)"
 git submodule init
 git submodule update
+git submodule foreach git pull --rebase origin master
 [ ! -e submodules/ministat/src/ministat ] && ( cd submodules/ministat &&
   ./configure &&
   make
