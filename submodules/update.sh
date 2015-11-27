86cb169c06a6768cf566a7275dc7d68ec6c359ba
Eitan Adler <lists@eitanadler.com>
2015-11-26 21:28:09 -0500

    Bump submodules

--- submodules/update.sh
+++ submodules/update.sh
@@ -3,7 +3,3 @@ cd "$(git rev-parse --show-toplevel)"
 git submodule init
 git submodule update
 git submodule foreach git pull --rebase origin master
-[ ! -e submodules/ministat/src/ministat ] && ( cd submodules/ministat &&
-  ./configure &&
-  make
-)
