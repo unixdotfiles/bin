94ccb469db37d0b43b1766b3c2a3ba3e32b3224d
Eitan Adler <lists@eitanadler.com>
2015-11-30 15:44:36 -0500

    update updater script

--- submodules/update.sh
+++ submodules/update.sh
@@ -3,3 +3,5 @@ cd "$(git rev-parse --show-toplevel)"
 git submodule init
 git submodule update
 git submodule foreach git pull --rebase origin master
+git add submodules
+git commit -m "bump submodules (automated)"
