d60555e6c0c41926e23a5c349c56db8562055a7c
Eitan Adler <lists@eitanadler.com>
2016-12-16 20:11:04 -0800

    move battery historian installer into smart script

--- submodules/install.sh
+++ submodules/install.sh
@@ -7,7 +7,3 @@ git submodule update --init
   make
 )
 [ ! -d submodules/go ] && mkdir submodules/go
-(
-  export GOPATH=~/bin/submodules/go/ &&
-  go get -d -u github.com/google/battery-historian/...
-)
