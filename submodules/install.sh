302cadb2019e224d0bf6df4f44a36521ab15e271
Eitan Adler <lists@eitanadler.com>
2016-12-16 20:15:21 -0800

    move ministat installer into a smart script

--- submodules/install.sh
+++ submodules/install.sh
@@ -1,9 +1,4 @@
 set -eux
 cd "$(git rev-parse --show-toplevel)"
 git submodule update --init
-[ ! -e submodules/ministat/src/ministat ] && (
-  cd submodules/ministat &&
-  ./configure &&
-  make
-)
 [ ! -d submodules/go ] && mkdir submodules/go
