8b42a5a681d4d3fce026017a46aefad21a91b55b
Eitan Adler <lists@eitanadler.com>
2016-12-25 14:19:11 -0800

    now that I don't use pex; don't bother with packages anymore

--- nmap/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='wnmap',
-      author='Eitan Adler',
-      author_email='lists@eitanadler.com',
-      scripts=['wnmap.py'],
-      license='MIT',
-      packages=find_packages()
-      )
