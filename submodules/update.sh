f0e8f305284e9e02a9550bfd4d55a61449da2be3
Eitan Adler <lists@eitanadler.com>
2017-11-01 01:40:37 -0700

    init not required with update --init

--- submodules/update.sh
+++ submodules/update.sh
@@ -2,7 +2,6 @@
 set -eux
 cd "$(git rev-parse --show-toplevel)"
 git pull --rebase
-git submodule init
 git submodule update --init --recursive
 git submodule foreach git pull --rebase origin master
 git add submodules
